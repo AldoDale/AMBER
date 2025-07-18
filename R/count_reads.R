19b16d0d3483a48df10e105c21a910acf478411a
AldoDale <aldo.d.alessandro@hotmail.it>
2025-07-18 13:45:05 +0200

    a

--- R/count_reads.R
+++ R/count_reads.R
@@ -19,8 +19,9 @@ setMethod("count_reads",
               y = totValues
             ))+
               ggplot2::geom_col(fill = "#b08169", color = "black") +
-              #theme_Aldo+
-              ggplot2::scale_y_continuous(expand = ggplot2::expansion(mult = c(0.005, .2)))
+              theme_Aldo+
+              ggplot2::scale_y_continuous(expand = ggplot2::expansion(mult = c(0.005, .2)))+
+              labs(x = "Sample", y = "nReads")
 
 
             object <- list(data = sort(totValues, decreasing = F), plot = plt)
